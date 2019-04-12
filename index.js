c534f8b4d2c8a9c81ea543e6f2459233ece64ff1
Talky\Batushka <talky.egor@gmail.com>
2019-04-12 17:35:51 +0300

    fix: styles and form

--- index.js
+++ index.js
@@ -1,39 +1,28 @@
-// const input = document.getElementById("input");
-// const output = document.getElementById("output");
-// const selin =  document.getElementById("selin");
-// const selout =  document.getElementById("selout");
-// document.getElementById("get").addEventListener("click", count);
-// function count() {
-//     fetch(`https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=${selout.value}&date=20190401&json`)
-//         .then((res) => res.json())
-//         .then((data) => {
-//             const course = data[0].rate;
-//             const uah = input.value;
-//             output.value = uah / course;
-//         })
-// }
-const input = document.getElementById("input");
-const output = document.getElementById("output");
-const selin =  document.getElementById("selin");
-const selout =  document.getElementById("selout");
-document.getElementById("get").addEventListener("click", count);
+const amount = document.getElementById("inputAmount");
+const result = document.getElementById("inputResult");
+const inputfromto = document.getElementById("inputFromTo");
+document.getElementById("convert").addEventListener("click", count);
 function count() {
+    event.preventDefault();
     fetch(`https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=3`)
         .then((res) => res.json())
         .then(data => {
-            if (selin.value === "UAH") {
-                const element = data.filter(l => l.ccy === selout.value);
-                const uah = input.value;
+            let input = inputfromto.value;
+            let fromto = input.split("/");
+
+            if (fromto[0] === "UAH") {
+                const element = data.filter(l => l.ccy === fromto[1]);
+                const uah = amount.value;
                 const course = element[0].sale;
-                const result = uah / course;
-                output.value = parseInt(result * 100) / 100;
+                const result1 = uah / course;
+                result.value = parseInt(result1 * 100) / 100;
             }
             else {
-                const element = data.filter(l => l.ccy === selin.value);
-                const uah = input.value;
+                const element = data.filter(l => l.ccy === fromto[0]);
+                const uah = amount.value;
                 const course = element[0].buy;
-                const result = uah * course;
-                output.value = parseInt(result * 100) / 100;
+                const result1 = uah * course;
+                result.value = parseInt(result1 * 100) / 100;
             }
         })
 }
